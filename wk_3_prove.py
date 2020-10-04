a4102e41105596f2bb5fcf7b0d9c09d82574ac48
ddemille1 <ddemille1@gmail.com>
2020-10-03 22:33:42 -0400

    finished wk4 checkpoint and wk3/4 prove

--- wk_3_prove.py
+++ wk_3_prove.py
@@ -1,10 +1,22 @@
-child_cost=float(input("\nWhat is the price of a child's meal? "))
+import os
+os.system('cls')
+
+child_cost = float(input("\nWhat is the price of a child's meal? "))
 #\n so the program isn't all bunched up at the top of the screen and is easier to read.
 #used "" because there is a ' in the sentence.
-adult_cost=float(input("What is the price of an adult's meal? "))
-child_number=int(input('How many children are there? '))
-adult_number=int(input('How many adults are there? '))
-tax_rate=float(input('What is the sales tax rate? '))
-subtotal=(child_cost*child_number + adult_cost*adult_number)
-print(f'\nSubtotal: ${subtotal}')
+adult_cost = float(input("What is the price of an adult's meal? "))
+child_number = int(input('How many children are there? '))
+adult_number = int(input('How many adults are there? '))
+tax_rate = float(input('What is the sales tax rate? '))
+#is it possible to put a % after the user's input?
 
+subtotal = (child_cost*child_number + adult_cost*adult_number)
+print(f'\nSubtotal: ${subtotal:.2f}')
+sales_tax = tax_rate * subtotal / 100
+print(f'Sales Tax: ${sales_tax:.2f}')
+total = subtotal + sales_tax
+print(f'Total: ${total:.2f}')
+
+payment = float(input('\nWhat is the payment amount? '))
+change = payment - total
+print(f'Change: ${change:.2f}')
