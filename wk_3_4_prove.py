34f9cacfe921011a84af68d4d7772156bf0db351
ddemille1 <ddemille1@gmail.com>
2020-10-07 22:07:46 -0400

    added stretch activity

--- wk_3_4_prove.py
+++ wk_3_4_prove.py
@@ -7,14 +7,18 @@ child_cost = float(input("\nWhat is the price of a child's meal? "))
 adult_cost = float(input("What is the price of an adult's meal? "))
 child_number = int(input('How many children are there? '))
 adult_number = int(input('How many adults are there? '))
+drink_cost = float(input('What is the price of a drink? '))
+drink_number = int(input('How many drinks are there? '))
+tip_rate = float(input('What percent would you like to tip? '))
 tax_rate = float(input('What is the sales tax rate? '))
 #is it possible to put a % after the user's input?
 
-subtotal = (child_cost*child_number + adult_cost*adult_number)
+subtotal = (child_cost * child_number + adult_cost * adult_number + drink_cost * drink_number)
 print(f'\nSubtotal: ${subtotal:.2f}')
 sales_tax = tax_rate * subtotal / 100
 print(f'Sales Tax: ${sales_tax:.2f}')
-total = subtotal + sales_tax
+tip = tip_rate * subtotal / 100
+total = subtotal + sales_tax + tip
 print(f'Total: ${total:.2f}')
 
 payment = float(input('\nWhat is the payment amount? '))
