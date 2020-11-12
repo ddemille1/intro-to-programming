52c854678eeb7776f2dd2b8e1acde2d8a17e8734
ddemille1 <ddemille1@gmail.com>
2020-11-11 21:15:59 -0500

    wk9 prove and teach completed

--- wk9_prove.py
+++ wk9_prove.py
@@ -1,7 +1,25 @@
 import os
 os.system('cls')
 
-item_name[] 
+print('Welcome to the Shopping Cart Program!\n')
+item_names = [] 
 repeat = True
 while repeat:
-    print('Welcome to the Shopping Cart Program!/n')
+    print('Please select one of the following: ')
+    print('1. Add item')
+    print('2. View Cart')
+    print('3. Remove item')
+    print('4. Compute total')
+    print('5. Quit')
+    action = int(input('plese enter an action: '))
+    if action == 1:
+        item = input('What item would you like to add? ')
+        item_names.append(item)
+        print(f'{item} has been added to the cart.\n')
+    elif action == 2:
+        for item in item_names:
+            print(item)
+        print()
+    elif action == 5:
+        print('Thank you. Good bye.')
+        repeat = False
--- wk9_teach.py
+++ wk9_teach.py
@@ -11,13 +11,23 @@ while number != 0:
     numbers.append(number)
     count = count + 1
 
+numbers.remove(0)
 sum = 0
 for number in numbers:
     sum += number
 
-average = sum/(count - 1)
+average = sum/(count-1)
 
 print(f'The sum is: {sum}')
-print(f'The average is: {average}')
-print(f'The largest nubmer is: {max(numbers)}')
+print(f'The average is: {average:.3f}')
+print(f'The largest number is: {max(numbers)}')
+print(f'The smallest positive number is: {min([number for number in numbers if number > 0])}')
+print('The sorted list is: ')
+numbers.sort()
+for number in numbers:
+    print(number)
+
+
+
+
 
